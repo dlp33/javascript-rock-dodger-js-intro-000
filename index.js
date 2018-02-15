7418bc4bd1d09d74aad778762acbd345cd5c6ba0
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-15 19:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,14 +111,13 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
+  if (e.which === LEFT_ARROW) {
+    moveDodgerLeft()
+  }
+
+  if(e.which === RIGHT_ARROW) {
+  	moveDodgerRight()
+  }
 }
 
 function moveDodgerLeft() {
