024accd5440b501c7b873ba690227f5d8c296f52
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-15 21:41:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const myInterval = setInterval(moveRock, 500)
  */
 function endGame() {
   //gameInterval = null;
-  ROCKS.length = 0;
+  ROCKS.splice(0,ROCKS.length);
   //window.removeEventListener('keydown', moveDodger);
   alert('YOU LOSE!');
 }
